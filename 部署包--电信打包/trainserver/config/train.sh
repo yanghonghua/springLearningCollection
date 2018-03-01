dae96e97a1bc04a7f3e23d6137de5d4a73b9e46c
871662532 <871662532@qq.com>
2018-03-01 11:10:52 +0800

    深交所项目需要trainserver、statis、diy打包及配置

---
+++ 部署包--电信打包/trainserver/config/train.sh
@@ -0,0 +1,105 @@
+JAVA_HOME="/usr/java/jdk1.8.0_144"
+RUNNING_USER=water
+APP_HOME=/usr/local/xyz/trainserver
+APP_MAINCLASS=Train-Server-0.0.1-SNAPSHOT.jar
+JAVA_OPTS="-ms512m -mx512m -Xmn256m -Djava.awt.headless=true -XX:MaxPermSize=128m"
+# jar包运行状态判断标准
+psid=0
+
+checkpid() {
+   javaps=`$JAVA_HOME/bin/jps -l | grep $APP_MAINCLASS`
+   if [ -n "$javaps" ]; then
+      psid=`echo $javaps | awk '{print $1}'`
+   else
+      psid=0
+   fi
+}
+
+start() {
+   checkpid
+   if [ $psid -ne 0 ]; then
+      echo "================================"
+      echo "warn: $APP_MAINCLASS already started! (pid=$psid)"
+      echo "================================"
+   else
+      echo -n "Starting $APP_MAINCLASS ..."
+      JAVA_CMD="cd $APP_HOME; $nohup $JAVA_HOME/bin/java $JAVA_OPTS  -jar $APP_MAINCLASS >/dev/null 2>&1 &"
+      su - $RUNNING_USER -c "$JAVA_CMD"
+      checkpid
+      if [ $psid -ne 0 ]; then
+         echo "(pid=$psid) [OK]"
+      else
+         echo "[Failed]"
+      fi
+   fi
+}
+ 
+
+stop() {
+   checkpid
+   if [ $psid -ne 0 ]; then
+      echo -n "Stopping $APP_MAINCLASS ...(pid=$psid) "
+      su - $RUNNING_USER -c "kill -9 $psid"
+      if [ $? -eq 0 ]; then
+         echo "[OK]"
+      else
+         echo "[Failed]"
+      fi
+ 
+      checkpid
+      if [ $psid -ne 0 ]; then
+         stop
+      fi
+   else
+      echo "================================"
+      echo "warn: $APP_MAINCLASS is not running"
+      echo "================================"
+   fi
+}
+ 
+status() {
+   checkpid
+ 
+   if [ $psid -ne 0 ];  then
+      echo "$APP_MAINCLASS is running! (pid=$psid)"
+   else
+      echo "$APP_MAINCLASS is not running"
+   fi
+}
+ 
+info() {
+   echo "System Information:"
+   echo "****************************"
+   echo `head -n 1 /etc/issue`
+   echo `uname -a`
+   echo
+   echo "JAVA_HOME=$JAVA_HOME"
+   echo `$JAVA_HOME/bin/java -version`
+   echo
+   echo "APP_HOME=$APP_HOME"
+   echo "APP_MAINCLASS=$APP_MAINCLASS"
+   echo "****************************"
+}
+ 
+case "$1" in
+   (start)
+      start
+      ;;
+   (stop)
+     stop
+     ;;
+   (restart)
+     stop
+     start
+     ;;
+   (status)
+     status
+     ;;
+   (info)
+     info
+     ;;
+  (*)
+     echo "Usage: $0 {start|stop|restart|status|info}"
+     exit 1
+     ;;
+esac
